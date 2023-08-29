bac1647d713154fdde2e286da7dcf2b60bec81b9
Muhammad-Usama123 <118450716+Muhammad-Usama123@users.noreply.github.com>
2023-08-29 14:58:55 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// not recommended
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// not recommended
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// not recommended
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
